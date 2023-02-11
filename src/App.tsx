98a8aba5dbe20dd0188db8111dda701a2b1bb653
kittypitty <samutic40@gmail.com>
2023-02-11 23:14:45 +0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NotFound } from './pages/NotFound';
+import { Deploy } from './pages/Deploy';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<NotFound />} />
-      <Route />
+      <Route path="/deploy" element={<Deploy />} />
     </Routes>
   );
 }
